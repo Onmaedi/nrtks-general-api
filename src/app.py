321e94aacca53458cab24c919379e889b96fda03
João Victor <joaovictorslf@gmail.com>
2020-10-22 20:50:15 -0300

    test route added

--- src/app.py
+++ src/app.py
@@ -5,5 +5,9 @@ from .config.configuration import init_configuration
 def create_app():
     app: Flask = Flask(__name__)
 
+    @app.route("/")
+    def index():
+        return "Test route"
+
     init_configuration(app)
     return app
